37cd78a94cfcd6bb7f521ff35493f560c511ddb2
Altair Sossai <altair_sossai@outlook.com>
2020-09-03 20:40:31 -0300

    Inicio do projeto

---
+++ Zeats.Legacy.IP.UnitTest/IpValidatorTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zeats.Legacy.IP.UnitTest
+{
+    [TestClass]
+    public class IpValidatorTest
+    {
+        [TestMethod]
+        public void IsIpV4()
+        {
+            Assert.IsFalse(IpValidator.IsIpV4(null));
+            Assert.IsFalse(IpValidator.IsIpV4(string.Empty));
+
+            Assert.IsFalse(IpValidator.IsIpV4("Lorem"));
+            Assert.IsFalse(IpValidator.IsIpV4("10"));
+            Assert.IsFalse(IpValidator.IsIpV4("10.20"));
+            Assert.IsFalse(IpValidator.IsIpV4("10.20.30"));
+            Assert.IsFalse(IpValidator.IsIpV4("10.20.30.256"));
+            Assert.IsFalse(IpValidator.IsIpV4("10.a.30.40"));
+
+            for (var i = 0; i < 256; i += 7)
+            for (var j = 0; j < 256; j += 7)
+            for (var k = 0; k < 256; k += 7)
+            for (var l = 0; l < 256; l += 7)
+                Assert.IsTrue(IpValidator.IsIpV4($"{i}.{j}.{k}.{l}"));
+        }
+    }
+}
---
+++ Zeats.Legacy.IP/IpValidator.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Zeats.Legacy.IP
+{
+    public static class IpValidator
+    {
+        public static bool IsIpV4(string ip)
+        {
+            if (string.IsNullOrWhiteSpace(ip))
+                return false;
+
+            var values = ip.Split('.');
+
+            return values.Length == 4 && values.All(r => byte.TryParse(r, out _));
+        }
+    }
+}
